f0184893c0279c166b4ee98a9de869c1506504c3
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-02-21 14:32:45 +0100

    Updated the fill/run LUT

--- EventAnalyzer/interface/FillNumberLUTHandler.h
+++ EventAnalyzer/interface/FillNumberLUTHandler.h
@@ -1,5 +1,5 @@
-#ifndef RecoCTPPS_ProtonProducer_FillNumberLUTHandler_h
-#define RecoCTPPS_ProtonProducer_FillNumberLUTHandler_h
+#ifndef DiphotonAnalyzer_EventAnalyzer_FillNumberLUTHandler_h
+#define DiphotonAnalyzer_EventAnalyzer_FillNumberLUTHandler_h
 
 #include <fstream>
 
@@ -7,36 +7,36 @@
 
 namespace CTPPSAlCa
 {
-    class FillNumberLUTHandler
-    {
-        public:
-            inline FillNumberLUTHandler( const char* file ):
-                valid_( false )
-            {
-                std::ifstream f( file );
-                if ( !f.is_open() ) return;
-                std::string ss;
-                while ( f >> ss ) {
-                    size_t pos = ss.find( ":" );
-                    if ( pos==std::string::npos ) continue;
-                    unsigned int run  = atoi( ss.substr( 0, pos ).c_str() ),
-                                 fill = atoi( ss.substr( pos+1, ss.size() ).c_str() );
-                    fillInRun_[run] = fill;
-                }
-                valid_ = true;
-            }
+  class FillNumberLUTHandler
+  {
+    public:
+      inline FillNumberLUTHandler( const char* file ):
+        valid_( false )
+      {
+        std::ifstream f( file );
+        if ( !f.is_open() ) return;
+        std::string ss;
+        while ( f >> ss ) {
+          size_t pos = ss.find( ":" );
+          if ( pos==std::string::npos ) continue;
+          unsigned int run  = atoi( ss.substr( 0, pos ).c_str() ),
+                       fill = atoi( ss.substr( pos+1, ss.size() ).c_str() );
+          fillInRun_[run] = fill;
+        }
+        valid_ = true;
+      }
 
-            inline unsigned int getFillNumber( unsigned int run ) const {
-                if ( !valid_ or !fillInRun_.count( run ) ) return 0;
-                return fillInRun_.at( run );
-            }
+      inline unsigned int getFillNumber( unsigned int run ) const {
+        if ( !valid_ or !fillInRun_.count( run ) ) return 0;
+        return fillInRun_.at( run );
+      }
 
-            inline bool isValid() const { return valid_; }
+      inline bool isValid() const { return valid_; }
 
-        private:
-            bool valid_;
-            std::map<unsigned int,unsigned int> fillInRun_;
-    };
+    private:
+      bool valid_;
+      std::map<unsigned int,unsigned int> fillInRun_;
+  };
 }
 
 #endif
